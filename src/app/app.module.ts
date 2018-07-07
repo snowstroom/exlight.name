a64b03c94b9f9e9bebbeb0a416919a3cfc89ad31
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-07-07 21:20:48 +0700

    pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RightComponent } from './components/right/right.component';
+import { AudioPlayerComponent } from './components/audio-player/audio-player.component';
+import { MainComponent } from './pages/main/main.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { VideoComponent } from './pages/video/video.component';
+import { MusicComponent } from './pages/music/music.component';
+import { PhotoComponent } from './pages/photo/photo.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    RightComponent,
+    AudioPlayerComponent,
+    MainComponent,
+    CatalogComponent,
+    VideoComponent,
+    MusicComponent,
+    PhotoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
