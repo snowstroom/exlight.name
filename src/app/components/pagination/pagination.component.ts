f657213236fc8b3d6086b1ae9f36e69786123d6b
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-07-08 19:17:32 +0700

    Pagination logic

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
+  @Input() itemCount: number;
+  @Input() onPage: number;
+  public activePage = 1;
+  public items: number[] = [1, 2, 3, 4, 5, 6];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toPage(page: number) {
+    this.activePage = page;
+  }
+
+  public nextPage() {
+    if (this.activePage < this.items.length) {
+      this.activePage++;
+    }
+  }
+
+  public prevPage() {
+    if (this.activePage > 1) {
+      this.activePage--;
+    }
   }
 
 }
