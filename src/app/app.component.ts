3db60e4165f5fdb56ca353cddb7e1be6645ae1ed
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-07-18 18:51:24 +0700

    HTML player add to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('htmlPlayer') htmlPlayer: ElementRef<HTMLAudioElement>;
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.htmlPlayer);
+    // this.htmlPlayer.nativeElement.play();
+  }
 }
