f29927c4175a6aa23ced7fdb2e2434f6a37607a6
eXlight <kerpith@gmail.com>
2019-03-12 01:04:26 +0700

    Client: Categories animation (active)

--- client/src/app/classes/categories.ts
+++ client/src/app/classes/categories.ts
@@ -6,10 +6,24 @@ export interface ICategoriesItem {
 
 export class CategoriesItem implements ICategoriesItem {
     public readonly id: number = this.__data.id;
+    public readonly itemWidth: number;
     public categoryName: string = this.__data.categoryName;
     public categoryRoute: string = this.__data.categoryRoute;
 
     public isActive: boolean;
 
-    constructor(private __data: ICategoriesItem) {}
+    constructor(private __data: ICategoriesItem) {
+        const element = document.createElement('div');
+        const wrapper = document.createElement('div');
+        wrapper.style.display = 'flex';
+        const body = document.getElementsByTagName('body').item(0);
+        element.style.padding = '8px 10px';
+        element.style.fontSize = '1rem';
+        element.style.fontFamily = 'Amatic SC, cursive';
+        element.innerText = this.categoryName;
+        wrapper.appendChild(element);
+        body.appendChild(wrapper);
+        this.itemWidth = element.clientWidth;
+        body.removeChild(wrapper);
+    }
 }
--- client/src/app/components/catalog-menu/catalog-menu.component.ts
+++ client/src/app/components/catalog-menu/catalog-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { CategoriesItem } from '@app/classes/categories';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ex-catalog-menu',
@@ -7,13 +8,45 @@ import { CategoriesItem } from '@app/classes/categories';
   styleUrls: ['./catalog-menu.component.scss']
 })
 export class CatalogMenuComponent {
-  @Input() public categories: CategoriesItem[] = [];
+  public categoriesItems: CategoriesItem[] = [];
   @Input() public showMenu: boolean;
   @Input() public catalogName: string;
   @Input() public currentpage: number;
   @Output() public selectedCat = new EventEmitter<CategoriesItem>();
 
+  public leftDec = 1;
+  public widthDec: number;
+
+  constructor(
+    private router: Router,
+    private actRouter: ActivatedRoute
+  ) {
+    this.actRouter.params.subscribe(params => this.calcDecorator(params.cat));
+  }
+
+  @Input() set categories(items: CategoriesItem[]) {
+    this.categoriesItems = items;
+    const param = this.router.url.split('/')[2];
+    this.calcDecorator(param);
+  }
+
   public selectCategory(category: CategoriesItem): void {
     this.selectedCat.emit(category);
+  }
+
+  public calcDecorator(category: string): void {
+    console.warn(this.categoriesItems);
+    let isFind = false;
+    let offsetLeft = 0;
+    this.categoriesItems.forEach((item, i) => {
+      if (category === item.categoryRoute) {
+        this.widthDec = item.itemWidth;
+        isFind = true;
+      }
+      if (!isFind) {
+        offsetLeft += item.itemWidth;
+      }
+    });
+    this.leftDec = offsetLeft;
   }
 }
