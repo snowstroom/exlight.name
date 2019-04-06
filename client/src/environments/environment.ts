f0708ca9627fc06d1a61c6afa4bfe15f139529b4
eXlight <kerpith@gmail.com>
2019-04-06 15:46:19 +0700

    Client: test manifest. Server: CORS, port, host

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  domain: 'http://127.0.0.1:8081'
+  domain: 'http://192.168.1.200:1050'
 };
 
 /*
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,10 +9,18 @@ const app = express();
 app.use(json());
 
 app.use(cors({
-    origin: ['http://127.0.0.1:4200', 'http://localhost:4200']
+    origin: [
+        'http://127.0.0.1:4200',
+        'http://localhost:4200',
+        'http://127.0.0.1:80',
+        'http://localhost:80',
+        'http://127.0.0.1',
+        'http://localhost',
+        'http://192.168.1.200'
+    ]
 }));
 
 app.use('/', ...routes);
 
-app.listen(PORT, 'localhost',
+app.listen(PORT, '0.0.0.0',
     () => console.log(`Server is start! Port ${PORT}`));
