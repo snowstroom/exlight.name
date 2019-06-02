d7c9644f1e45c0e94bcd05435ad9a829b77b8488
eXlight <kerpith@gmail.com>
2019-06-02 23:32:50 +0700

    Server: get user info

--- server/src/controllers/user/user.controller.ts
+++ server/src/controllers/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Put, Param, Body, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Inject, Put, Param, Body, HttpException, HttpStatus, Get } from '@nestjs/common';
 import { USER } from '../../consts/provider-names';
 import { Repository } from 'typeorm';
 import { User, IUser } from '../../models/user.model';
@@ -6,6 +6,22 @@ import { User, IUser } from '../../models/user.model';
 @Controller({ path: 'api/user' })
 export class UserController {
     constructor(@Inject(USER) private userRep: Repository<User>) { }
+
+    @Get('/:id')
+    public async getUserInfo(@Param() param: any) {
+        try {
+            const res = await this.userRep.findOne(param.id, {
+                select: ['id', 'email', 'firstname', 'secondname', 'roleId'],
+            });
+            if (res) {
+                return res;
+            } else {
+                throw new HttpException({ error: ''}, HttpStatus.NOT_FOUND);
+            }
+        } catch (err) {
+            throw new HttpException({ error: err }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @Put('/:id')
     public async updateUser(@Param() params: any, @Body() user: Partial<IUser>): Promise<void> {
