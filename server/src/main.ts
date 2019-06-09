0e918454e108ae81ae215bcebb29233618fe6aca
eXlight <kerpith@gmail.com>
2019-06-09 13:48:38 +0700

    Server: delete envinit from main.ts, add cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { parse } from 'dotenv';
-import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  switch (process.env.NODE_ENV) {
-    case 'prod': {
-      const cnfg = parse(readFileSync(`${__dirname}/configs/prod.env`));
-      process.env = {...process.env, ...cnfg };
-      break;
-    }
-    case 'dev': {
-      const cnfg = parse(readFileSync(`${__dirname}/configs/dev.env`));
-      process.env = {...process.env, ...cnfg };
-      break;
-    }
-    default: throw new Error();
-  }
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:4200',
+      'http://localhost:4200',
+      'http://127.0.0.1:80',
+      'http://localhost:80',
+      'http://127.0.0.1',
+      'http://localhost',
+      'http://192.168.1.200',
+    ],
+  });
   await app.listen(process.env.EXPRESS_PORT);
 }
 bootstrap();
