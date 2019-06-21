7a0b215fd176bebafea4091605e6a4f4b20dcc01
eXlight <kerpith@gmail.com>
2019-06-21 19:29:46 +0700

    Server: roles list

--- server/src/controllers/role/role.controller.ts
+++ server/src/controllers/role/role.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Inject, Post, Body, Delete, HttpStatus, HttpException, Param, Put, SetMetadata, UseGuards } from '@nestjs/common';
+import { Controller, Inject, Post, Body, Delete, HttpStatus, HttpException, Param, Put, SetMetadata, UseGuards, Get } from '@nestjs/common';
 import { ROLE } from 'src/consts/provider-names';
 import { Role, IRole } from 'src/models/role.model';
 import { Repository, ObjectLiteral } from 'typeorm';
 import { E_ENTITY_TYPES } from 'src/enums/entity-types';
-import { META_ENTITY_KEY, META_ACCESS_KEY } from 'src/consts/meta-keys';
-import { CREATE, UPDATE, DELETE } from 'src/consts/access';
+import { META_ENTITY_KEY, META_ACCESS_KEY, META_PUBLIC_KEY } from 'src/consts/meta-keys';
+import { CREATE, UPDATE, DELETE, READ } from 'src/consts/access';
 import { AuthGuardService } from 'src/guards/auth.guard';
 
 @Controller({ path: '/api/role' })
@@ -13,6 +13,19 @@ export class RoleController {
     constructor(
         @Inject(ROLE) private roleRep: Repository<Role>,
     ) { }
+
+    @Get('/list')
+    @SetMetadata(META_PUBLIC_KEY, true)
+    @SetMetadata(META_ACCESS_KEY, READ)
+    @SetMetadata(META_ACCESS_KEY, E_ENTITY_TYPES.role)
+    public async getList(): Promise<any> {
+        try {
+            const dbRes = await this.roleRep.find({ relations: ['access'] });
+            return dbRes;
+        } catch (err) {
+            throw new HttpException({ error: err }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @Post('/')
     @SetMetadata(META_ACCESS_KEY, CREATE)
