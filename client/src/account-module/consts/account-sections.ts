65d8c52bb296e15892dc3fab0dad5875487454ae
Kernichnyy Andrey <only.exlight@gmail.com>
2019-08-09 01:48:30 +0700

    Client: add account section arrays

---
+++ client/src/account-module/consts/account-sections.ts
@@ -0,0 +1,21 @@
+import { ICategory } from '@account-module/interfaces/category';
+
+export const PROFILE_SECTIONS: ICategory[] = [{
+    name: 'Профиль',
+    route: 'example',
+    template: 'example/%/example'
+}, {
+    name: 'Настройки',
+    route: 'example',
+    template: 'example/%/example'
+}];
+
+export const ARTICLES_SECTIONS: ICategory[] = [{
+    name: 'Создать',
+    route: 'example',
+    template: 'example/%/example'
+}, {
+    name: 'Мои статьи',
+    route: 'example',
+    template: 'example/%/example'
+}];
