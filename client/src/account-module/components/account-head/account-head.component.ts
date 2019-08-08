7cb582d6abce8143ab6915a5858f2ba640e4c5ed
Kernichnyy Andrey <only.exlight@gmail.com>
2019-08-09 01:47:00 +0700

    Client: modify account head component

--- client/src/account-module/components/account-head/account-head.component.ts
+++ client/src/account-module/components/account-head/account-head.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { CatalogMenuComponent } from '@common-module/components/catalog-menu/catalog-menu.component';
+import { Component, Input } from '@angular/core';
+import { Category } from '@account-module/models/category';
+import { ICategory } from '@account-module/interfaces/category';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'ex-account-head',
@@ -7,4 +9,26 @@ import { CatalogMenuComponent } from '@common-module/components/catalog-menu/cat
     styleUrls: ['account-head.component.scss']
 })
 
-export class AccountHeadComponent extends CatalogMenuComponent { }
+export class AccountHeadComponent {
+    public categoriesItems: Category[] = [];
+    @Input() public catalogName: string;
+
+    public active: number;
+
+    constructor(
+        protected router: Router,
+        private actRouter: ActivatedRoute
+    ) {
+        this.actRouter.params.subscribe(params => this.calcDecorator(params.cat));
+    }
+
+    @Input() set categories(items: ICategory[]) {
+        this.categoriesItems = items.map(i => new Category(i));
+        const param = this.router.url.split('/')[2];
+        this.calcDecorator(param);
+    }
+
+    public calcDecorator(category: string): void {
+        this.active = this.categoriesItems.findIndex(item => category === item.route);
+    }
+}
