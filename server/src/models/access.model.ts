2c2a3130522bc4e56e24fb2962a059937e520287
Kernichnyy Andrey <only.exlight@gmail.com>
2019-09-06 00:06:31 +0700

    SERVER: access model change

--- server/src/models/access.model.ts
+++ server/src/models/access.model.ts
@@ -6,5 +6,5 @@ export class Access implements AccessNamespace.IAccess {
     @PrimaryGeneratedColumn() public id: number;
     @Column() public entity: string | AccessNamespace.E_ENTITY_TYPES;
     @Column() public access: number;
-    @Column({ type: 'varchar' }) public description: string;
+    @Column({ type: 'varchar', nullable: true }) public description: string;
 }
