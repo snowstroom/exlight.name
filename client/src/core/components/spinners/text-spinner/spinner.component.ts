19af10d90948df44dd3c1d7b8db70c3645dd0467
Kernichnyy Andrey <only.exlight@gmail.com>
2020-03-09 19:06:40 +0700

    Update text spinner

--- client/src/core/components/spinners/text-spinner/spinner.component.ts
+++ client/src/core/components/spinners/text-spinner/spinner.component.ts
@@ -1,31 +1,31 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
-    selector: 'ex-spinner',
-    styleUrls: ['spinner.component.scss'],
-    templateUrl: 'spinner.component.html'
+  selector: 'ex-spinner',
+  styleUrls: ['spinner.component.scss'],
+  templateUrl: 'spinner.component.html',
 })
 export class SpinnerComponent implements OnInit, OnDestroy {
-    public visibleText: string;
-    public spinnerText = '...';
-    @Input() public wait: boolean;
-    private curLit = 0;
-    private timer: NodeJS.Timer;
+  public visibleText: string;
+  public spinnerText = '...';
+  @Input() public wait: boolean;
+  @Input() public size = 2.2;
+  private curLit = 0;
+  private timer: NodeJS.Timer;
 
-    public ngOnInit(): void {
-        this.timer = setInterval(() => {
-            this.visibleText = `${this.spinnerText.slice(0, this.curLit)} _`;
-            if (this.curLit <= this.spinnerText.length) {
-                this.curLit++;
-            } else {
-                this.visibleText = '';
-                this.curLit = 0;
-            }
-        }, 300);
-    }
+  public ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.visibleText = `${this.spinnerText.slice(0, this.curLit)} _`;
+      if (this.curLit <= this.spinnerText.length) {
+        this.curLit++;
+      } else {
+        this.visibleText = '';
+        this.curLit = 0;
+      }
+    }, 300);
+  }
 
-    public ngOnDestroy(): void {
-        clearInterval(this.timer);
-    }
-
+  public ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
 }
