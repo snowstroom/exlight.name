4b584020680521ee7655873f35dff41ac105c42f
Kernichnyy Andrey <only.exlight@gmail.com>
2020-03-13 01:32:53 +0700

    Update commentary tree

--- client/src/article-module/components/comments-tree/comments-tree.component.ts
+++ client/src/article-module/components/comments-tree/comments-tree.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Commentary } from '@article-module/models/commentary';
-import { UserApi } from '@account-module/models/api/user';
+import {
+  TCommentControllType,
+  ICommentSendEvent,
+} from '../comment-control/comment-control.component';
 
 const PADDING = 55;
 @Component({
@@ -9,13 +12,22 @@ const PADDING = 55;
   styleUrls: ['comments-tree.component.scss'],
 })
 export class CommentTreeComponent {
-  public rootComment = new Commentary();
+  public activeEdit = false;
+  public root = new Commentary();
   @Input() public waitComments = false;
-  @Input() public comments: Commentary[] = [];
-  @Input() public isAuth: boolean;
-  @Output() public delete = new EventEmitter<Commentary>();
+  @Input() public currentUserId: number;
+  @Input() public total = 0;
   @Output() public new = new EventEmitter<Commentary>();
   @Output() public update = new EventEmitter<Commentary>();
+  @Output() public delete = new EventEmitter<Commentary>();
+  @Output() public answer = new EventEmitter<Commentary>();
+  @Output() public more = new EventEmitter();
+  @Output() public like = new EventEmitter<Commentary>();
+  @Output() public dislike = new EventEmitter<Commentary>();
+
+  @Input() set comments(val: Commentary[]) {
+    this.root.comments = val;
+  }
 
   public getPadding(lvl: number): number {
     return PADDING * lvl;
@@ -30,38 +42,70 @@ export class CommentTreeComponent {
     comment.editable = true;
   }
 
-  public cancelEdit(comment: Commentary): void {
-    comment.editable = false;
+  public cancelEdit(type: TCommentControllType, comment: Commentary): void {
+    if (type === 'answer') {
+      this.deleteActiveEdit(comment.parentComment);
+    } else {
+      comment.editable = false;
+    }
   }
 
   public answerToComment(comment: Commentary): void {
-    const asnwer = new Commentary({
+    this.deleteActiveEdit(this.root);
+    const answer = new Commentary({
       articleId: comment.articleId,
       comment: '',
       comments: [],
       parentComment: comment,
       createDate: new Date(),
-      updateDate: new Date()
+      updateDate: new Date(),
     });
-    asnwer.editable = true;
-    comment.comments.push(asnwer);
+    comment.comments.push(answer);
   }
 
-  public sendNewComment(content: string, comment?: Commentary): void {
-    if (comment) {
-      if (!comment.editable) {
-        comment.comment = content;
-        comment.updating = true;
-        this.new.emit(comment);
-      } else {
-        comment.updating = true;
-        comment.comment = content;
-        this.update.emit(comment);
+  public deleteActiveEdit(comment: Commentary): void {
+    comment.comments = comment.comments.filter(c => {
+      this.deleteActiveEdit(c);
+      return !!c.id;
+    });
+  }
+
+  public updateEditChanges(
+    { content, type }: ICommentSendEvent,
+    comment: Commentary,
+  ): void {
+    switch (type) {
+      case 'new': {
+        this.root.updating = true;
+        this.root.comment = content;
+        this.new.emit(this.root);
+        break;
       }
-    } else {
-      this.rootComment.updating = true;
-      this.rootComment.comment = content;
-      this.new.emit(this.rootComment);
+      case 'edit': {
+        comment.updating = true;
+        comment.comment = content;
+        comment.edited = true;
+        this.update.emit(comment);
+        break;
+      }
+      case 'answer': {
+        comment.comment = content;
+        comment.updating = true;
+        this.answer.emit(comment);
+        break;
+      }
     }
+  }
+
+  public loadMore(): void {
+    this.more.emit();
+  }
+
+  public likeComment(comment: Commentary): void {
+    this.like.emit(comment);
+  }
+
+  public dislikeComment(comment: Commentary): void {
+    this.dislike.emit(comment);
   }
 }
